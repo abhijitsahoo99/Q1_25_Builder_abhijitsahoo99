78c65ad74e2ed1b587c08a8b3051eddbdfbd9525
abhijitsahoo99 <abhijitsahooas.01@gmail.com>
2025-01-08 16:56:13 +0530

    first assignment

---
+++ airdrop/transfer.ts
@@ -0,0 +1,59 @@
+import {
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  Transaction,
+  sendAndConfirmTransaction,
+  SystemProgram,
+} from "@solana/web3.js";
+import wallet from "./dev-wallet.json";
+
+const from = Keypair.fromSecretKey(new Uint8Array(wallet));
+const to = new PublicKey("7k43x5ejbKW8pwsGq7eAeEa7J8WL1FCTTut5bnMfGvta");
+
+const connection = new Connection("https://api.devnet.solana.com");
+
+(async () => {
+  try {
+    const balance = await connection.getBalance(from.publicKey);
+    console.log(`Balance: ${balance}`);
+
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: from.publicKey,
+        toPubkey: to,
+        lamports: balance,
+      })
+    );
+    transaction.recentBlockhash = (
+      await connection.getLatestBlockhash("confirmed")
+    ).blockhash;
+    transaction.feePayer = from.publicKey;
+
+    const fee =
+      (
+        await connection.getFeeForMessage(
+          transaction.compileMessage(),
+          "confirmed"
+        )
+      ).value || 0;
+
+    transaction.instructions.pop();
+
+    transaction.add(
+      SystemProgram.transfer({
+        fromPubkey: from.publicKey,
+        toPubkey: to,
+        lamports: balance - fee,
+      })
+    );
+    const signature = await sendAndConfirmTransaction(connection, transaction, [
+      from,
+    ]);
+    console.log(`Success! Check out your TX here:
+        https://explorer.solana.com/tx/${signature}?cluster=devnet`);
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
+})();
